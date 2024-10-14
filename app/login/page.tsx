c7af6b4f06654149c4a7754c60675011a7a0c6a8
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-14 13:29:56 +0800

    add router to list of dependencies

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -59,7 +59,7 @@ export default function Login() {
         router.push("/");
       }
     },
-    [username, password, isStudent]
+    [username, password, isStudent, router]
   );
 
   useEffect(() => {
