702ad65153c97564ffcfe3274267954a52331301
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-31 10:13:09 +0800

    feat: add AuthContextProvider

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Afacad } from "next/font/google";
 import "./globals.scss";
 import Page from "@/components/Page";
+import { AuthContextProvider } from "@/components/AuthContextProvider";
 
 const font = Afacad({
   weight: ["400", "500", "600", "700"],
@@ -19,10 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${font.className}`}>
-        <Page>{children}</Page>
-      </body>
-    </html>
+    <AuthContextProvider>
+      <html lang="en">
+        <body className={`${font.className}`}>
+          <Page>{children}</Page>
+        </body>
+      </html>
+    </AuthContextProvider>
   );
 }
---
+++ components/AuthContextProvider.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { User } from "@/payload-types";
+import { createContext, useCallback, useContext, useState } from "react";
+
+type AuthContextType = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+  login: (email: string, password: string) => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = useCallback(async (email: string, password: string) => {
+    // Call the login API
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL || ""}/api/users/login`,
+      {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+      }
+    );
+
+    const data = await response.json();
+
+    // If the response is not ok, throw an error
+    if (!response.ok) {
+      throw new Error(data.error.message);
+    }
+
+    // Set the user state
+    setUser(data.user);
+  }, []);
+
+  const logout = useCallback(async () => {
+    // Call the logout API
+    await fetch(`${process.env.NEXT_PUBLIC_API_URL || ""}/api/users/logout`, {
+      method: "POST",
+    });
+
+    // Set the user state to null
+    setUser(null);
+  }, []);
+
+  // when component mounts, check if the user is logged in
+  useState(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL || ""}/api/users/me`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data.user);
+      })
+      .catch(() => {
+        setUser(null);
+      });
+  });
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthContextProvider");
+  }
+
+  return context;
+}
