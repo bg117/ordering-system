02f40dded4c1f05c4fbb4eb045abf0cf98ebaa66
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-12-28 13:12:39 +0800

    feat: allow user to change quantity of orders

--- app/(app)/cart/page.tsx
+++ app/(app)/cart/page.tsx
@@ -5,44 +5,72 @@ import CartItem from "@/components/CartItem";
 import ExtraInstructionsCard from "@/components/ExtraInstructionsCard";
 import { CartItem as CartItemType, Item } from "@/payload-types";
 import { api } from "@/utilities/api";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { PaginatedDocs } from "payload";
+import { useCallback } from "react";
 import { Row, Col, Card } from "react-bootstrap";
 
 export default function CartPage() {
   const { user } = useAuth();
   const router = useRouter();
+  const queryClient = useQueryClient();
 
-  const { data, isLoading, isError, error } = useQuery<PaginatedDocs<CartItemType>>({
+  const { data, isLoading, isError, error } = useQuery<
+    PaginatedDocs<CartItemType>
+  >({
     queryKey: ["cart-items"],
     queryFn: async () => {
       const response = await api("/cart-items", { method: "GET" });
       return response;
-    }
+    },
   });
+
+  // mutation to update quantity of cart item
+  const { mutate } = useMutation({
+    mutationKey: ["cart-items"],
+    mutationFn: async ({ id, quantity }: { id: number; quantity: number }) => {
+      const response = await api(`/cart-items/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify({ quantity }),
+      });
+      return response;
+    },
+    onSuccess: () => {
+      return queryClient.invalidateQueries({ queryKey: ["cart-items"] });
+    },
+    onError: (error) => {
+      throw error;
+    },
+  });
+
+  const cartItems = data?.docs;
+  const calculateTotal = useCallback(() => {
+    if (!cartItems)
+      throw new Error("Cart items are not loaded yet. Please wait...");
+    return cartItems
+      .reduce(
+        (total, item) => total + (item.item as Item).price * item.quantity,
+        0
+      )
+      .toFixed(2);
+  }, [cartItems]);
 
   if (user === null) {
     router.push("/login");
   }
 
   if (isLoading || !data || user === undefined) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   if (isError) {
     throw error;
   }
 
-  const cartItems = data.docs;
-
-  if (cartItems.length === 0 || !cartItems) {
-    return <div>Your cart is empty</div>
+  if (cartItems?.length === 0 || !cartItems) {
+    return <div>Your cart is empty</div>;
   }
-
-  const calculateTotal = () => {
-    return cartItems.reduce((total, item) => total + (item.item as Item).price * item.quantity, 0).toFixed(2);
-  };
 
   return (
     <div className="container mt-5">
@@ -51,10 +79,20 @@ export default function CartPage() {
           <Card>
             <Card.Body>
               <Card.Title>Order Summary</Card.Title>
-              <Card.Subtitle className="mb-3 text-muted">Review your order details</Card.Subtitle>
+              <Card.Subtitle className="mb-3 text-muted">
+                Review your order details
+              </Card.Subtitle>
               <div>
                 {cartItems.map((item, index) => (
-                  <CartItem key={index} name={(item.item as Item).name} quantity={item.quantity} price={(item.item as Item).price} />
+                  <CartItem
+                    key={index}
+                    name={(item.item as Item).name}
+                    quantity={item.quantity}
+                    price={(item.item as Item).price}
+                    onChangeQuantity={(quantity) => {
+                      mutate({ id: item.id, quantity });
+                    }}
+                  />
                 ))}
               </div>
               <hr />
@@ -73,4 +111,3 @@ export default function CartPage() {
     </div>
   );
 }
-
--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -4,12 +4,28 @@ type CartItemProps = {
   name: string;
   quantity: number;
   price: number;
+  onChangeQuantity: (quantity: number) => void;
 };
 
-const CartItem: React.FC<CartItemProps> = ({ name, quantity, price }) => {
+const CartItem: React.FC<CartItemProps> = ({
+  name,
+  quantity,
+  price,
+  onChangeQuantity,
+}) => {
   return (
     <div className="d-flex justify-content-between mb-2">
-      <div>{name} x {quantity}</div>
+      <div>
+        <input
+          type="number"
+          defaultValue={quantity}
+          style={{
+            width: "50px",
+          }}
+          onBlur={(e) => onChangeQuantity(Number.parseInt(e.target.value))}
+        ></input>
+        {name}
+      </div>
       <div>₱{(price * quantity).toFixed(2)}</div>
     </div>
   );
