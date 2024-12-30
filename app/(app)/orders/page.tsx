2b9cd224a21437d438371cf41106ec007b51954d
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-12-30 19:40:35 +0800

    feat: make orders sort of realtime

--- app/(app)/orders/page.tsx
+++ app/(app)/orders/page.tsx
@@ -5,16 +5,23 @@
 import QueueOrders from "@/components/QueueOrders"; // Adjust path if needed
 import { api } from "@/utilities/api";
 import { useQuery } from "@tanstack/react-query";
+import qs from "qs";
 
 export default function OrdersPage() {
+  const query = qs.stringify(
+    { sort: "placedAt", order: "desc", depth: 3 },
+    { addQueryPrefix: false }
+  );
+
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["orders"],
     queryFn: async () => {
-      const response = await api("/orders", {
+      const response = await api(`/orders?${query}`, {
         method: "GET",
       });
       return response;
     },
+    refetchInterval: 1000,
   });
 
   if (isLoading) return <span>Loading...</span>;
