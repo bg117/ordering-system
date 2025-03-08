39586c6677040833e3214a09ea5902728cd850fa
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2025-03-08 20:49:24 +0800

    fix: cart item only for user

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -3,7 +3,14 @@
 import { api } from "@/utilities/api";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useCallback } from "react";
-import { Card, Button, CardBody, CardImg, CardText, CardTitle } from "react-bootstrap";
+import {
+  Card,
+  Button,
+  CardBody,
+  CardImg,
+  CardText,
+  CardTitle,
+} from "react-bootstrap";
 import { useAuth } from "./AuthContextProvider";
 import { useRouter } from "next/navigation";
 import qs from "qs";
@@ -31,7 +38,11 @@ export default function MenuCard({
   // add item to cart
   const { mutate, isPending, isError, error } = useMutation({
     mutationKey: ["cart-items", id],
-    mutationFn: async (newItem: { user: number, item: number, quantity: number }) => {
+    mutationFn: async (newItem: {
+      user: number;
+      item: number;
+      quantity: number;
+    }) => {
       const response = await api("/cart-items", {
         body: JSON.stringify(newItem),
       });
@@ -39,26 +50,40 @@ export default function MenuCard({
     },
     onSuccess: () => {
       return queryClient.invalidateQueries({ queryKey: ["cart-items"] });
-    }
-  })
+    },
+  });
 
-  const cartItemQuery = qs.stringify({
-    where: {
-      item: {
-        equals: id
-      }
-    }
-  }, { addQueryPrefix: false });
+  const cartItemQuery = qs.stringify(
+    {
+      where: {
+        and: [
+          {
+            user: {
+              equals: user?.id,
+            },
+          },
+          {
+            item: {
+              equals: id,
+            },
+          },
+        ],
+      },
+    },
+    { addQueryPrefix: false }
+  );
 
   // check if item is already in the cart
   // if it is, disable the button
   const { data: cartItems } = useQuery({
     queryKey: ["cart-items", id],
     queryFn: async () => {
-      const response = await api(`/cart-items/count?${cartItemQuery}`, { method: "GET" });
+      const response = await api(`/cart-items/count?${cartItemQuery}`, {
+        method: "GET",
+      });
       return response;
-    }
-  })
+    },
+  });
 
   const addNewItem = useCallback(() => {
     if (!user) {
@@ -87,10 +112,12 @@ export default function MenuCard({
               ₱{price.toFixed(2)}
             </span>
           )}
-          <Button variant="primary"
+          <Button
+            variant="primary"
             onClick={addNewItem}
-            disabled={isPending || !!user && cartItems?.totalDocs > 0}
-            aria-disabled={isPending || !!user && cartItems?.totalDocs > 0}>
+            disabled={isPending || (!!user && cartItems?.totalDocs > 0)}
+            aria-disabled={isPending || (!!user && cartItems?.totalDocs > 0)}
+          >
             {user && cartItems?.totalDocs > 0
               ? "Item in Cart"
               : isPending
